014d116b16dd44ca105d17204aa934b5b0eb3490
xuzepei <xuzepei@gmail.com>
2024-11-06 09:30:06 +0800

    Init

---
+++ app/src/main/java/com/tool/demo/MainActivity.java
@@ -0,0 +1,90 @@
+package com.tool.demo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.tool.monthpicker.MonthPickerDialog;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView monthTV;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        monthTV = findViewById(R.id.month_tv);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("####", "Clicked the show button");
+                showMonthPicker();
+            }
+        });
+    }
+
+    void showMonthPicker() {
+        // 获取当前日期
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        MonthPickerDialog.Builder builder = new MonthPickerDialog.Builder(MainActivity.this, new MonthPickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(int selectedMonth, int selectedYear) {
+                monthTV.setText(String.valueOf(selectedYear) + "-" + String.valueOf(selectedMonth));
+            }
+        }, year, month);
+
+        builder.setActivatedMonth(Calendar.JULY)
+                .setActivatedYear(year)
+                .setMinYear(1999)
+                .setMaxYear(year)
+                //.setMinMonth(Calendar.JANUARY)
+                .setTitle("Select Month")
+                .setOnMonthChangedListener(new MonthPickerDialog.OnMonthChangedListener() {
+                    @Override
+                    public void onMonthChanged(int selectedMonth) {
+
+                    }
+                })
+                .setOnYearChangedListener(new MonthPickerDialog.OnYearChangedListener() {
+                    @Override
+                    public void onYearChanged(int year) {
+
+                    }
+                }).build().show();
+                //.setMonthRange(Calendar.FEBRUARY, Calendar.NOVEMBER)
+                // .setMaxMonth(Calendar.OCTOBER)
+                // .setYearRange(1890, 1890)
+                // .setMonthAndYearRange(Calendar.FEBRUARY, Calendar.OCTOBER, 1890, 1890)
+                //.showMonthOnly()
+                // .showYearOnly()
+                //.setOnMonthChangedListener(new MonthPickerDialog.OnMonthChangedListener() {
+
+
+    }
+}
